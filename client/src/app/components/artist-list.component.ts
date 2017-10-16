588f4ac4b31cd34dc0fccd51ae484e58267ba49f
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-10-16 14:28:00 -0500

    20171610 CSC: Leer el listado de artistas

--- client/src/app/components/artist-list.component.ts
+++ client/src/app/components/artist-list.component.ts
@@ -3,36 +3,82 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { GLOBAL } from '../services/global';
 import { UserService } from '../services/user.service';
+import { ArtistService } from '../services/artist.service';
+
 import { Artist } from '../models/artist';
 
 @Component({
   selector: 'artist-list',
   templateUrl: '../views/artist-list.html',
-  providers: [UserService]
+  providers: [UserService, ArtistService]
 })
 
 export class ArtistListComponent implements OnInit {
   public titulo: string;
-  public artist: Artist[];
+  public artists: Artist[];
   public identity;
   public token;
   public url: string;
+  public next_page;
+  public prev_page;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _artistService: ArtistService
   ) {
-    this.titulo = 'Artistas!!';
+    this.titulo = 'Artistas';
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.url = GLOBAL.url;
-    console.log(this.identity);
-    console.log(this.token);
+    this.next_page = 1;
+    this.prev_page = 1;
+
   }
 
   ngOnInit() {
     console.log('artist-list.component.ts cargado');
+    this.getArtists();
     //Conseguir el listado de artistas
   }
+
+  getArtists() {
+    this._route.params.forEach(
+      (params: Params) => {
+        let page = + params['page'];
+        if (page) {
+          page = 1;
+        } else {
+          this.next_page = page + 1;
+          this.prev_page = page - 1;
+
+          if (this.prev_page == 0) {
+            this.prev_page = 1;
+          }
+        }
+        this._artistService.getArtists(this.token, page).subscribe(
+          response => {
+            if (!response.artists) {
+              this._router.navigate(['/']);
+            } else {
+              this.artists = response.artists;
+
+            }
+          },
+          error => {
+            var errorMessage = <any>error;
+            if (errorMessage != null) {
+              var body = JSON.parse(error._body);
+              console.log(error);
+            }
+          }
+        );
+
+
+      });
+  }
+
+
+
 }
